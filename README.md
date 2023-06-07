0626dcc42ca42d45f23a0f8748a824771a8bc2e4
irenevg18 <irenevalleguillen@gmail.com>
2023-06-07 14:26:29 +0200

    setting up the activity

--- README.md
+++ README.md
@@ -1,2 +1,71 @@
-# Loops
-Actividad 12
+> Loops
+**Actividad 12**
+
+# Loops #
+
+![imagen_de_portada](https://www.cronj.com/blog/wp-content/uploads/Javascript-loops.png)
+
+En esta actividad practicaremos como crear y utilizar loops(bucles).
+
+## Objetivos ##
+
+- Crear **bucles**.
+- Utilizar **contadores**,**condiciones** y **iteradores**.
+- Crear un loop **do while**, un **while** y un **for**.
+- Subir nuestra versión a [GitHub](https://github.com/)
+
+
+
+---
+
+### **Iniciamos la actividad** ###
+
+1. Entrar en la carpeta donde vamos a ubicar la actividad:
+    1. Abre la **terminal**.
+    2. Con los comandos `cd` y `cd ..` navegamos por nuestro ordenador hasta llegar a la dirección dónde queremos ubicar la actividad.
+
+2. Abrir en enlace de la actividad en el navegador.
+3. Clonar la actividad:
+    1. Copiamos el link *https* del botón *Clone* de nuestro **Github**.
+    2. Vamos a la **terminal** y, dentro de la carpeta donde queremos ubicar la actividad, escribimos el comando `git clone + (pegamos el link que habíamos copiado)`.
+
+4. Abrir la actividad en el **Visual Studio Code**
+    1. Entramos dentro de la carpeta que se ha generado con el comando `git clone`, haciendo `cd + (el nombre de la carpeta clonada)`.
+    2. Dentro de la carpeta, escribimos el comando `code .` para abrir esta carpeta en el **VSC**.
+
+---
+
+### :point_right: **Ejercicio** ###
+
+- Trabajaremos el archivo **challenges.js**.
+- Abriremos el archivo **index.html** en el navegador.
+- Veremos el resultado de los ejercicios a través de la **consola**.
+    - *Para abrir la consola haremos `cmd`+ `alt`+ `j` en mac o `shift`+ `j` en windows y Linux.*
+- Cuando hayamos terminado de contestar un ejercicio, lo **comentamos** con `//` o `/* */`para que no interfira con el resto.
+
+
+---
+
+### **Mientras trabajamos en la actividad** ###
+
+Mientras trabajamos en la actividad es muy importante ir guardando los cambios que vamos haciendo. Este **proceso de guardado** será el siguiente: 
+
+1. El clásico `ctrl + s` o `cmd + s` en nuestro archivo del **VSC**.
+2. Abrimos la **terminal**, comprobamos que nos encontramos en la ruta correcta y escribimos los siguientes comandos:
+    1. `git add .`
+    2. `git commit -m "mensaje"`
+    3. `git push`
+
+3. Vamos al navegador y comprobamos que se ha hecho correctamente en nuestro **Github**.
+
+
+---
+
+### Recursos ###
+
+- [Recurso 1](https://www.dofactory.com/javascript/loops)
+
+- [Recurso 2](https://developer.mozilla.org/en-US/docs/Web/JavaScript/Guide/Loops_and_iteration)
+
+
+
---
+++ challenges.js
@@ -0,0 +1,210 @@
+
+/* Ejercicio 1 -------
+
+Arregla la expresión do while del siguiente loop para que por consola
+se impriman los números del 1 al 10.
+*/
+
+
+let count = 0;
+
+do {
+  console.log(10);
+  count++;
+}
+while (count <= 10);
+
+
+
+
+
+
+
+/* Ejercicio 2 -------
+
+Crea un loop que cuente desde 0 hasta 100.
+*/
+
+
+//tu código aquí
+
+
+
+
+
+
+
+/* Ejercicio 3 -------
+
+Crea un loop que cuente desde 100 hasta 0.
+*/
+
+
+
+
+//tu código aquí
+
+
+
+
+
+
+
+
+/* Ejercicio 4 -------
+
+Añade un console.log dentro del siguiente loop para que imprima
+los numeros del 0 al 4.
+*/
+
+
+
+const start = 5;
+const end = 0;
+
+while (start > end) {
+  end++;
+}
+
+
+
+
+
+
+
+/* Ejercicio 5 -------
+
+Añade una expresión JavaScript dentro del ${} que imprima la diferencia
+entre la capacidad y el número de personas.
+
+*/
+
+
+// const people = 1;
+// const capacity = 12;
+
+// for (let people = 1; people <= capacity; people++) {
+//   console.log(`There is room for ${} more guests in the building`);
+// }
+
+
+
+
+/* Ejercicio 6 -------
+
+Imprime todos los números PARES entre el 0 y el 50.
+
+Pista: Podemos aumentar el iterador de 2 en 2.
+*/
+
+
+
+//tu código aquí
+
+
+
+
+
+
+/* Ejercicio 7 -------
+
+Ahora crea un for loop que imprima en la consola todos los número 
+de 0 a 50.
+
+Añade un "if/else" dentro del loop. Si el número (x) es par, imprimer:
+`The number ${x} is even`.
+Si es impar, imprime: `The number ${x} is odd`.
+
+*/
+
+
+
+
+
+/* Ejercicio 8 -------
+
+------------- *** ¡Vamos a probar algo nuevo! *** -------------
+Para seleccionar un carácter dentro de un "string", utilizamos 
+".charAt()" e insertamos dentro de este paréntesis el número del 
+caracter al que queremos acceder (la posición en la que se encuentra 
+dentro de ese string).
+
+Por ejemplo: "Sofia".charAt(0) // "S"
+             "Sofia".charAt(2) // "f"
+
+El método ".toUpperCase()" combierte las letras a mayúscula.
+
+Haz UN cambio en el siguiente código para que se imprima la frase inicial
+alternando mayúsculas y minúsculas
+*/
+
+
+
+// const sentence = "i am shouting";
+// const numberOfChars = 12;
+// let result = "";
+// let i = 0;
+
+// while (i <= numberOfChars) {
+//   let letter = sentence.charAt(0);
+//   if (i % 2 === 0) {
+//     letter = letter.toUpperCase();
+//   }
+//   result += letter;
+//   i++;
+// }
+
+// console.log(result);
+
+
+
+
+
+
+/* Ejercicio 9 -------
+
+Escribe un programa que imprima todos los números del 1 al 100.
+
+Ahora, para todos los números que sea divisibles entre 3, imprime la palabra
+"Fizz" en vez del número.
+
+Para todos los que sean divisibles entre 5, imprime la palabra "Buzz".
+
+Y para todos aquellos números divisibles tanto entre 3 cómo entre 5,
+imprime la palabra "FizzBuzz".
+
+(pregunta típica de entrevista)
+
+*/
+
+
+
+
+//tu código aquí
+
+
+
+
+
+
+/* Ejercicio 10 -------
+
+Crea el siguiente triángulo escribiendo un bucle que llame 5 veces
+a un console.log:
+
+*
+**
+***
+****
+*****
+
+*/
+
+
+
+
+
+
+
+
+
